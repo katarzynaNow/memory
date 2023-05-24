e8cd2b6a8d9ceb26633ece42b89f31f1261370c8
Katarzyna <noworyta.katarzyna@wp.pl>
2023-05-24 12:16:24 +0200

    category adding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
-import { NavigationComponent } from './ui/navigation/navigation.component';
-import { CategoriesComponent } from './features/categories/categories.component';
-import { CategoryCardComponent } from './ui/category-card/category-card.component';
+import { NavigationComponent } from "./ui/navigation/navigation.component";
+import { CategoriesComponent } from "./features/categories/categories.component";
+import { CategoryCardComponent } from "./ui/category-card/category-card.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [AppComponent, NavigationComponent, CategoriesComponent, CategoryCardComponent],
-  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    CategoriesComponent,
+    CategoryCardComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
