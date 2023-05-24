c92e47e8b3f461338ccec1353e6ef6a433bca3a5
Katarzyna <noworyta.katarzyna@wp.pl>
2023-05-24 13:33:50 +0200

    components routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { NavigationComponent } from "./ui/navigation/navigation.component";
 import { CategoriesComponent } from "./features/categories/categories.component";
 import { CategoryCardComponent } from "./ui/category-card/category-card.component";
 import { FormsModule } from "@angular/forms";
+import { FlashCardsComponent } from "./features/flash-cards/flash-cards.component";
+import { GamesComponent } from "./features/games/games.component";
 
 @NgModule({
   declarations: [
@@ -15,6 +17,8 @@ import { FormsModule } from "@angular/forms";
     NavigationComponent,
     CategoriesComponent,
     CategoryCardComponent,
+    FlashCardsComponent,
+    GamesComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FontAwesomeModule, FormsModule],
   providers: [],
